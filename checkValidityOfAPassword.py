4863f3f360cef63305c06ef2d3bd863705b8a057
namy112 <namratadhuru@gmail.com>
2018-02-05 18:02:49 -0500

    Program to check the validity of password input

---
+++ checkValidityOfAPassword.py
@@ -0,0 +1,37 @@
+""""A website requires the users to input username and password to register. Write a program to check the validity of password input by users.
+Following are the criteria for checking the password:
+1. At least 1 letter between [a-z]
+2. At least 1 number between [0-9]
+1. At least 1 letter between [A-Z]
+3. At least 1 character from [$#@]
+4. Minimum length of transaction password: 6
+5. Maximum length of transaction password: 12
+Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma.
+Example
+If the following passwords are given as input to the program:
+ABd1234@1,a F1#,2w3E*,2We3345
+Then, the output of the program should be:
+ABd1234@1"""
+import re
+
+passwords=['ABd1234@1','niahn' ,  'ANCa123#']
+outputList=[]
+for password in passwords:
+    
+    if len(password)<6 or len(password)>12:
+        continue
+    if not re.search("[a-z]", password):
+        continue
+    if not re.search("[a-z]", password):
+        continue 
+    if not re.search("[0-9]", password):
+        continue
+    if not re.search("[$#@]", password):
+        continue
+    
+    else:
+        outputList.append(password)
+        
+    
+        
+print("We winner are/is  %s " %(outputList))
